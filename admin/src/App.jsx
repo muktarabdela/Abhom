e5a3460b4ea28e018421763b1cde774bb3c300f0
muktar abdela <muktarhassen6@gmail.com>
2024-03-16 08:06:53 +0300

    initail vite admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,34 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect } from 'react'
+import { BrowserRouter, Routes, Route, } from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components'
+import {
+  Dashboard, Appointments, Calendar, Employee, Stacked, Pyramid, Customers,
+  Kanban, Area, Bar, Pie, Financial, ColorMapping, Editor, Properties, ColorPicker, Line
+} from './pages'
+const App = () => {
+  const activemenu = true
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <BrowserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button type="button" className="text-3xl  p-3 hover:drop-shadow-xl text-white hover:bg-light-gray" style={{ background: 'blue', borderRadius: '50%' }}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activemenu ? (
+            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+              <Sidebar />
+            </div>) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>)
+          }
+          <div
+            className={
+              `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activemenu ? 'md:ml-72' : 'flex-2'}`
+            }
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              <Navbar />
+            </div>
+
+          </div>
+
+          <div>
+            <Routes>
+              {/* dashboard  */}
+              <Route path="/" element={(<Dashboard />)} />
+              <Route path="/ecommerce" element={(<Dashboard />)} />
+
+              {/* pages  */}
+              <Route path="/appointments" element={<Appointments />} />
+              <Route path="/employees" element={<Employee />} />
+              <Route path="/customers" element={<Customers />} />
+
+              {/* apps  */}
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+
+              {/* charts  */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
+
+            </Routes>
+          </div>
+
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ admin/src/pages/Charts/ColorMapping.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ColorMapping = () => {
+  return (
+    <div>Color Mapping</div>
+  )
+}
+
+export default ColorMapping
